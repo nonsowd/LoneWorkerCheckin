3f0a7e19c3ca1003fa241a20e61f92558737ab9f
Chinonso Asuzu <147524533+nonsowd@users.noreply.github.com>
2024-06-06 20:37:48 +0100

    Add an MVC client application

--- Src/LoneWorkerCheckin.AppHost/Program.cs
+++ Src/LoneWorkerCheckin.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.LoneWorkerCheckin_Api>("loneworkercheckin-api");
 
+builder.AddProject<Projects.LoneWorkerCheckin_MVC>("loneworkercheckin-mvc");
+
 builder.Build().Run();

08a0aeaed368c5de94990c0960ff54215066f599
Chinonso Asuzu <147524533+nonsowd@users.noreply.github.com>
2024-08-08 19:22:40 +0100

    Deleted MVC application

--- Src/LoneWorkerCheckin.MVC/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace LoneWorkerCheckin.MVC.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}

14c472139e09e068c0179a36e0a23774cfabd3e1
Chinonso Asuzu <147524533+nonsowd@users.noreply.github.com>
2024-11-07 20:33:55 +0000

    Upgraded to Aspire 9

--- Src/LoneWorkerCheckin.AppHost/Program.cs
+++ Src/LoneWorkerCheckin.AppHost/Program.cs
@@ -1,15 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var db = builder.AddSqlServer("loneworkercheckin-sqlserver")
-    .WithHealthCheck()
+    .WithLifetime(ContainerLifetime.Persistent)
     .AddDatabase("loneworkercheckin-db", "loneworkercheckin");
 
 var loneworkercheckinserviceapi = builder.AddProject<Projects.LoneWorkerCheckin_Api>("loneworkercheckin-api")
-    .WithReference(db).WaitFor(db)
-    .WithHealthCheck();
+    .WithReference(db).WaitFor(db);
 
 builder.AddProject<Projects.LoneWorkerCheckin_Blazor>("loneworkercheckin-blazor")
-    .WithReference(loneworkercheckinserviceapi).WaitFor(loneworkercheckinserviceapi)
-    .WithHealthCheck();
+    .WithReference(loneworkercheckinserviceapi).WaitFor(loneworkercheckinserviceapi);
 
 builder.Build().Run();

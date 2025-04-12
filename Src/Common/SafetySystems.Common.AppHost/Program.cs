a1dfdaf57efc17a567c72479977f8affbfd45df3
Chinonso Asuzu <147524533+nonsowd@users.noreply.github.com>
2025-04-12 13:06:23 +0100

    Updated to .net 9 and added blazor safety audits

--- Src/Common/SafetySystems.Common.AppHost/Program.cs
+++ Src/Common/SafetySystems.Common.AppHost/Program.cs
@@ -10,4 +10,9 @@ var loneworkercheckinserviceapi = builder.AddProject<Projects.SafetySystems_Lone
 builder.AddProject<Projects.SafetySystems_LoneWorkerCheckin_Blazor>("loneworkercheckin-blazor")
     .WithReference(loneworkercheckinserviceapi).WaitFor(loneworkercheckinserviceapi);
 
+var safetyauditapi = builder.AddProject<Projects.SafetySystems_SafetyAudit_Api>("safetyaudit-api");
+
+builder.AddProject<Projects.SafetySystems_SafetyAudit_Blazor>("safetyaudit-blazor")
+    .WithReference(safetyauditapi).WaitFor(safetyauditapi);
+
 builder.Build().Run();

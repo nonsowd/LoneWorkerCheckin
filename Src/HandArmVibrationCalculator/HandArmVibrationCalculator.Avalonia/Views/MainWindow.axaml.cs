e08bf4e7a8d34df87de88b665ba7b8b49362f4da
Chinonso <147524533+nonsowd@users.noreply.github.com>
2025-07-19 13:19:08 +0100

    Added Avalonia application to the project

---
+++ Src/HandArmVibrationCalculator/HandArmVibrationCalculator.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace HandArmVibrationCalculator.Avalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}

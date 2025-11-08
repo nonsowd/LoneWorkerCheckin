8bfe5d86eec9f73f951728369453f2b63201ac03
Chinonso <147524533+nonsowd@users.noreply.github.com>
2025-11-08 12:08:52 +0000

    Added migration services (WIP)

---
+++ Src/LoneWorkerCheckin/SafetySystems.LoneWorkerCheckin.DBMigrationServices/Program.cs
@@ -0,0 +1,7 @@
+using SafetySystems.LoneWorkerCheckin.DBMigrationServices;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
